3d50377adaa48c9ed67cfc4e47219fc386d248d4
Blake Mitchell <38139493+BlakeMitchell829@users.noreply.github.com>
2018-07-10 20:34:27 -0500

    unit-4-game

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,49 +1,36 @@
-$(document).ready(function() {
-
 var randomResult;
 var lost = 0;
 var win = 0;
 var score = 0;
 
-var resetAndStart = function () 
+function resetAndStart() {
 
-    $(".crystals").empty();
-
-    var images = [
-        https://cdn.pixabay.com/photo/2015/11/22/19/01/pink-diamond-1056757_960_720.png
-
-    randomResult = Math.floor(Math.random() * 69) + 30;
-
-    console.log(randomResult);
-
-  //generates four crystals with random numbers
-  $("#result").html('Magic Number: ' + randomResult);
-  //four crystals with four random numbers and the div for the random number
-
-  for (var i = 0; i < 4; i++) {
-
-    var random = Math.floor(Math.random() * 11) + 1;
-
-    console.log(random);
-
-    var crystal = $("<div>");
-    crystal.attr({
-      "class": 'crystal',
-      "data-random": random
-    });
-    crystal.css({
-      "background-image": "url('" + images[i] + "')",
-      "background-size": "cover"
-    });
-
-    // crystal.html(random);
-
-    $(".crystals").append(crystal);
-
-  }
+  var crystalbuttons = $(".crystal").children();
+  $.map(crystalbuttons, (thiscrystal) => {
+    $(thiscrystal).attr("data-random", Math.floor(Math.random() * 11) + 1);
+  });
+  randomResult = Math.floor(Math.random() * 69) + 30;
+  $("#number").html(randomResult);
+  console.log(randomResult);
 }
+//generates four crystals with random numbers
+$(".results").attr('results', randomResult);
 
-$("#previous").html(previous);
+//four crystals with four random numbers and the div for the random number
+
+// var crystal = $(".crystal").children();
+// for (var i = 0; i < 4; i++) {
+
+//   var random = 
+//   console.log(random);
+//   $(crystal[i]).attr("data-random", random);
+//   // crystal.html(random);
+
+//   // $(".crystal").append(crystal);
+
+// }
+
+$("#score").html(score);
 
 resetAndStart();
 
@@ -53,38 +40,30 @@ $(document).on('click', ".crystal", function () {
 
   var num = parseInt($(this).attr('data-random'));
 
-  previous += num;
+  score += num;
 
-  $("#previous").html(previous);
+  $("#games").html(score);
 
-  console.log(previous);
+  console.log(score);
 
-  if (previous > randomResult) {
+  if (score > randomResult) {
     lost++;
 
     $("#lost").html(lost);
 
-    previous = 0;
+    score = 0;
 
     resetAndStart();
 
-  } else if (previous === randomResult) {
+  } else if (score === randomResult) {
     win++;
 
     $("#win").html(win);
 
-    previous = 0;
+    games = 0;
 
 
     resetAndStart();
   }
 
-});
-  
-]
-
-randomResult = Math.floor(Math.random() * 69) + 30;
-
-console.log(randomResult);
-
-//generates four crystals with random nu
+})
